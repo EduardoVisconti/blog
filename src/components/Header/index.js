715774fb7b4f2c09ddb13309868ca42de8e4f15e
Eduardo Visconti <duduhsg@gmail.com>
2025-07-28 21:50:03 -0400

    styling menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,25 @@
-function Header () {
+import logosvg from '../../svg/logo.svg'
+
+const Header = () => {
+  const descLogo = 'This is the logo of the blog.'
+
   return (
-    <header>
-      <h1>Blog Header</h1>
-    </header>
-  );
+    <>
+      <header className='flex-space-between'>
+        <div className="logo">
+          <img src={logosvg} alt={descLogo} />
+        </div>
+        <div className="search">
+          <input type="text" className="input-search" name="Search..." />
+        </div>
+        <ul className="menu">
+          <li><a href="#categories" className='nav-link'>Categories</a></li>
+          <li><a href="#about" className='nav-link'>About</a></li>
+          <li><a href="#contact" className='nav-link'>Contact</a></li>
+        </ul>
+      </header>
+    </>
+  )
 }
 
-export default Header;
+export default Header
