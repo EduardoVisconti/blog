acb81408deef6caa629fce6c3c12bf50c519bd89
Eduardo Visconti <duduhsg@gmail.com>
2025-07-28 22:52:38 -0400

    articles styling

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,10 +2,22 @@ import Post from '../Post';
 
 const Article = () => {
   return (
-    <>
-      <h1>Article</h1>
-      <Post />
-    </>
+    <section className="container">
+      <h3>Articles</h3>
+      <div className="mt-5 container-posts">
+        <Post subtitle = "Technology" title = "Definitive Blog Guide">
+          This is a comprehensive guide to creating and managing a blog. It covers everything from choosing a platform to writing engaging content and promoting your blog effectively.
+        </Post>
+
+        <Post subtitle = "Photography" title = "Whats The Best Camera of 2021?">
+          The best camera of 2021 depends on your needs, whether you're a professional photographer or a casual user. This article explores the top options available this year.
+        </Post>
+
+        <Post subtitle = "Cinema" title = "The 10 Movies With The Highest Box Office">
+          The 10 movies with the highest box office earnings are a mix of franchises, adaptations, and original stories. This article takes a closer look at each film and what made it a success.
+        </Post>
+      </div>
+    </section>
   );
 }
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,7 +1,21 @@
-const Post = () => {
+const Post = ({ subtitle, title, children }) => {
   return (
-    <h1>Post</h1>
-  );
+    <div className="post mx-2">
+      <div className="img-post"></div>
+      <h5>{subtitle}</h5>
+      <h3>{title}</h3>
+      <p className="mt-1">
+        {children}
+      </p>
+      <div className="flex pt-2">
+        <div className="img-profile"></div>
+        <div className="desc-profile ml-2">
+          <h6 className="color-blue">Author Name</h6>
+          <p>Aug, 2023 - 8 min read</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Post;
+export default Post
